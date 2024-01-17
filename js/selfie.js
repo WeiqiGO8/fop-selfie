03b0572809c0009f52184462b23b028eaedb2312
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-01-17 13:04:25 +0100

    update variables

--- js/selfie.js
+++ js/selfie.js
@@ -7,25 +7,31 @@ let y = 200;
 let w = 200;
 let h = 200;
 
+push();
+translate(-100, -100);
+
 //head
 fill(226, 171, 83);
-ellipse(400, 400, 300);
+ellipse(x + 200, y + 200, w + 100);
 
 //eyes
-fill(colorWhite);
-ellipse(470, 350, 50);
-
-fill(colorBlack);
-ellipse(470, 350, 10);
 
 fill(colorWhite);
-ellipse(340, 350, 50);
+ellipse(x + 270, y + 150, w - 150);
 
 fill(colorBlack);
-ellipse(340, 350, 10);
+ellipse(x + 270, y + 150, w - 190);
+
+fill(colorWhite);
+ellipse(x + 140, y + 150, w - 150);
+
+fill(colorBlack);
+ellipse(x + 140, y + 150, w - 190);
 
 //mouth
 fill(colorWhite);
-rect(340, 460, 120, 40);
+rect(x + 140, y + 260, w - 80, h - 160, 100);
 
 //hair
+
+pop();

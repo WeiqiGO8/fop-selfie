c4f646b051c9a34d8ce805e678998e1662b0948e
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-01-17 14:54:53 +0100

    update
    removed folder and updated the x, y, width, height on rect(); and ellipse();

--- selfie.js
+++ selfie.js
@@ -8,29 +8,29 @@ let w = 200;
 let h = 200;
 
 push();
-translate(-100, -100);
+translate(10, 10);
 
 //head
 fill(226, 171, 83);
-ellipse(x + 200, y + 200, w + 100);
+ellipse(x + 50, y + 50, w + 100);
 
 //eyes
 
 fill(colorWhite);
-ellipse(x + 270, y + 150, w - 150);
+ellipse(x, y + 10, w - 150);
 
 fill(colorBlack);
-ellipse(x + 270, y + 150, w - 190);
+ellipse(x, y + 10, w - 190);
 
 fill(colorWhite);
-ellipse(x + 140, y + 150, w - 150);
+ellipse(x + 100, y + 10, w - 150);
 
 fill(colorBlack);
-ellipse(x + 140, y + 150, w - 190);
+ellipse(x + 100, y + 10, w - 190);
 
 //mouth
 fill(colorWhite);
-rect(x + 140, y + 260, w - 80, h - 160, 100);
+rect(x - 10, y + 100, w - 80, h - 160, 100);
 
 //hair
 

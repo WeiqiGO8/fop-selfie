c574a761e581fb37af414f5d8af0f49198fce0c5
WeiqiGO8 <hannah.lingzhi@gmail.com>
2024-01-17 12:33:34 +0100

    init commit

---
+++ js/selfie.js
@@ -0,0 +1,31 @@
+//variables
+let colorWhite = color(255, 255, 255);
+let colorBlack = color(0, 0, 0);
+
+let x = 200;
+let y = 200;
+let w = 200;
+let h = 200;
+
+//head
+fill(226, 171, 83);
+ellipse(400, 400, 300);
+
+//eyes
+fill(colorWhite);
+ellipse(470, 350, 50);
+
+fill(colorBlack);
+ellipse(470, 350, 10);
+
+fill(colorWhite);
+ellipse(340, 350, 50);
+
+fill(colorBlack);
+ellipse(340, 350, 10);
+
+//mouth
+fill(colorWhite);
+rect(340, 460, 120, 40);
+
+//hair
